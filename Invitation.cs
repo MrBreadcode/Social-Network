c7f5273ac7066351f87d811bbd89f0da2f2772e5
MrBreadcode <76493650+MrBreadcode@users.noreply.github.com>
2021-06-02 22:37:47 +0300

    Add files via upload

---
+++ Invitation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Social_Networks
+{
+    public class Invitation : UserEventsArgs
+    {
+        public Invitation(User sender, string message = null) : base(message, sender)
+        {
+            if (sender == null)
+                throw new ArgumentNullException("Sender must not be null");
+            if (message == null)
+                Message = DefaultMessage(sender);
+            else
+            {
+                if (message.Length > 100)
+                    throw new ArgumentException("Message must be no more than 100 characters");
+                Message = message;
+            }
+        }
+        private string DefaultMessage(User sender)
+        {
+            string message = $"Привет, мое имя {sender.GetUserName()} и ";
+            message += $"Я хочу добавить тебя в друзья";
+            return message;
+        }
+        public enum Respond
+        {
+            Accept,
+            Decline
+        }
+        internal User Sender { get { return user; } }
+    }
+}
